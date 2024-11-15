6db7cb87f589506942400e8cf22c8387381cabb2
ZanGrant <mgrantpangau@gmail.com>
2024-11-15 21:19:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+$(document).ready(function () {
+    // animation Script for display
+    const animationScript = ["Web Development", "UI/UX Design", "Game Development", "Data Analytics"];
+
+    // Typing animation script
+    new Typed(".typing", {
+      strings: animationScript,
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+    });
+  
+    new Typed(".typing2", {
+      strings: animationScript,
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+    });
+});
